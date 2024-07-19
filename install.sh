a88c58e64bcf61de7ccf6084287801a8ca92cb9e
sky5566jf <117685382+sky5566jf@users.noreply.github.com>
2024-07-20 02:41:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -143,7 +143,9 @@ install_x-ui() {
     wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
-    config_after_install
+    #econfig_after_install
+      /usr/local/x-ui/x-ui setting -username matisu0207 -password a5833576
+      /usr/local/x-ui/x-ui setting -port 33333
     #echo -e "如果是全新安装，默认网页端口为 ${green}54321${plain}，用户名和密码默认都是 ${green}admin${plain}"
     #echo -e "请自行确保此端口没有被其他程序占用，${yellow}并且确保 54321 端口已放行${plain}"
     #    echo -e "若想将 54321 修改为其它端口，输入 x-ui 命令进行修改，同样也要确保你修改的端口也是放行的"
